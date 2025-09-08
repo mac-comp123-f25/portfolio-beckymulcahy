663218b5f26babdac71a0692a50ecc0b1c66b589
beckymulcahy <bmulcahy@macalester.edu>
2025-09-08 10:36:10 -0500

    ICA 2

---
+++ src/ica/02-data_types/greet.py
@@ -0,0 +1,3 @@
+kind_person = 'Aunt Jodi'
+thank_you_note = 'Dear ' + kind_person + (' Thank you for the graduation present.' +  ' I can’t wait to use it.' + ' I hope you are enjoying your summer.' + ' You’re the best, Aunt Judi!' + ' Love, Mittens')
+print(thank_you_note)
