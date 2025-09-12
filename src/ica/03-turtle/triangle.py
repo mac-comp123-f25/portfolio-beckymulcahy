276d048a68edddc854d0619c5403c90537b71cab
beckymulcahy <bmulcahy@macalester.edu>
2025-09-12 10:13:13 -0500

    Create triangle.py

---
+++ src/ica/03-turtle/triangle.py
@@ -0,0 +1,12 @@
+import turtle
+window = turtle.Screen()
+my_turtle = turtle.Turtle()
+window.bgcolor("lavender")
+my_turtle.color("blue")
+my_turtle.forward(50)
+my_turtle.left(135)
+my_turtle.forward(35)
+my_turtle.left(90)
+my_turtle.forward(40)
+window.exitonclick() # this line closes the window when user clicks
+
