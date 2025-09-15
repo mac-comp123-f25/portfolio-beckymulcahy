44a4239a038307b469b802ee462253b61c7768d0
beckymulcahy <bmulcahy@macalester.edu>
2025-09-15 10:24:19 -0500

    Update tax.py

--- src/ica/04-functions/tax.py
+++ src/ica/04-functions/tax.py
@@ -1,5 +1,7 @@
 def add_tax(price, tax_rate):
+    print('price and tax rate inputs:', price, tax_rate)
     tax_amt = price * tax_rate
+    print(tax_amt, 'return value:', tax_amt)
     return price + tax_amt
 
 add_tax(25.99, 0.0725)
