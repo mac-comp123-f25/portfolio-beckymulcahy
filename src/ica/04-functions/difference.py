32793b860bce55af0f736818a794dd22288f9c4b
beckymulcahy <bmulcahy@macalester.edu>
2025-09-15 10:21:08 -0500

    updates

--- src/ica/04-functions/difference.py
+++ src/ica/04-functions/difference.py
@@ -3,8 +3,11 @@ def smallest_diff(x, y, z):
     diff2 = abs(y - z)
     diff3 = abs(x - z)
     min_diff = min(diff1, diff2, diff3)
+    print('smallest_diff inputs:', x, y, z)
+    print(diff1, diff2, diff3, 'return value:', min_diff)
     return min_diff
 smallest_diff(3, 9, 5)
+
 
 def smallest_diff(x, y, z):
     diff1 = abs(x - y)
---
+++ src/ica/04-functions/tax.py
@@ -0,0 +1,10 @@
+def add_tax(price, tax_rate):
+    tax_amt = price * tax_rate
+    return price + tax_amt
+
+add_tax(25.99, 0.0725)
+
+'''Local Environment 
+tax_amt = 25.99 * 0.0725
+'''
+
