3b29758ad656c74b03fb2a2b3270acb2783305f1
beckymulcahy <bmulcahy@macalester.edu>
2025-09-15 10:22:35 -0500

    Update difference.py

--- src/ica/04-functions/difference.py
+++ src/ica/04-functions/difference.py
@@ -1,9 +1,9 @@
 def smallest_diff(x, y, z):
+    print('smallest_diff inputs:', x, y, z)
     diff1 = abs(x - y)
     diff2 = abs(y - z)
     diff3 = abs(x - z)
     min_diff = min(diff1, diff2, diff3)
-    print('smallest_diff inputs:', x, y, z)
     print(diff1, diff2, diff3, 'return value:', min_diff)
     return min_diff
 smallest_diff(3, 9, 5)
