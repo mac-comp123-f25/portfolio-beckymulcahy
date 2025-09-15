1291df711d6604a2c9fecdc38cce0de44d088899
beckymulcahy <bmulcahy@macalester.edu>
2025-09-15 10:15:48 -0500

    Update difference.py

--- src/ica/04-functions/difference.py
+++ src/ica/04-functions/difference.py
@@ -5,3 +5,20 @@ def smallest_diff(x, y, z):
     min_diff = min(diff1, diff2, diff3)
     return min_diff
 smallest_diff(3, 9, 5)
+
+def smallest_diff(x, y, z):
+    diff1 = abs(x - y)
+    diff2 = abs(y - z)
+    diff3 = abs(x - z)
+    min_diff = min(diff1, diff2, diff3)
+    return min_diff
+smallest_diff(32, 43, 90)
+
+''' Local Environment 
+x  32
+y  43
+z  90 
+diff1= abs(32-43)
+diff2= abs(43-90)
+diff3= abs(32-90)
+'''
