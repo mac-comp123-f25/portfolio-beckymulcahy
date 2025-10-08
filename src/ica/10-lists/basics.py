cad4d995dcaae99b8a4e68fba676efcee212fb3c
beckymulcahy <bmulcahy@macalester.edu>
2025-10-08 10:22:33 -0500

    Update basics.py

--- src/ica/10-lists/basics.py
+++ src/ica/10-lists/basics.py
@@ -6,8 +6,10 @@ def every_other (list) :
 print(every_other([1, 2, 3]))
 
 def sum_positive (numbers)  :
-    result = 0
-    for i in range (0, len(numbers), 2) :
-        result += numbers[i]
-        if result > 0 :
-            break
+    total = 0
+    for num in numbers :
+        if num > 0 :
+            total += num
+        return total
+
+print(sum_positive([1, 2, 3, -5, -10]))
