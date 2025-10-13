e21908d093704da8e6414a4f1bbbe19879b4ac4e
beckymulcahy <bmulcahy@macalester.edu>
2025-10-13 10:31:56 -0500

    Update l_system.py

--- src/ica/09-strings/fractal/l_system.py
+++ src/ica/09-strings/fractal/l_system.py
@@ -5,10 +5,18 @@ Implementation of the Aristid Lindenmayer-system (L-systems)
 """
 
 
-def apply_l_system(axiom: str, rules: dict, n: int):
-    assert type(axiom) is str
-    assert type(rules) is dict
-    assert type(n) is int
+def apply_l_system(params: dict):
+    assert type(params) is dict
+    assert len(params) == 3
+    assert all(k in params for k in)
+
+    axiom = params['axiom']
+    rules = params['rules']
+    n = params['n']
+
+    assert type(axiom) is str, "'axiom' must be a string"
+    assert type(rules) is dict, "'rules' must be a dictionary"
+    assert type(n) is int, "'n' must be an integer"
 
     s = axiom
     for _ in range(n):
@@ -47,3 +55,14 @@ if __name__ == "__main__":
 
     for i in range(10):
         print(apply_l_system(axiom, rules, i))
+
+
+
+# def draw_hilbert_curve(tur, n, angle, step_size):
+#     hilbert_curve = { ‘axiom’: 'F+F+F+F',
+#                       ‘rules’: {'F': 'F+F-F-FF+F+F-F'},
+#                       ‘n’: n }
+#     l_system = apply_l_system(hilbert_curve)
+#
+#     draw_fractal(tur, l_system, angle, step_size)
+
