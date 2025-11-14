6ae2006b30430b1c00b06c972ed9d5607197abd8
beckymulcahy <bmulcahy@macalester.edu>
2025-11-14 10:36:10 -0600

    ica

---
+++ src/ica/16-simple-images/scale_color.py
@@ -0,0 +1,11 @@
+import imageTools
+
+
+def grayscale(pic):
+    new_pic = pic.copy()
+    for (x, y) in new_pic:
+        (r, g, b) = new_pic.getColor(x, y)
+        lumin = (r + g + b) / 3
+        new_pic.setColor(x, y, (lumin, lumin, lumin))
+
+    return new_pic
